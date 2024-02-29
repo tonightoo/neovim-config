428a218ca7c559969f2343b5ce13fa98072141fe
tonightoo <tonightoo.development@gmail.com>
2024-03-01 01:49:29 +0900

    add lsp plugins

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,23 @@
+return {
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	config = function()
+		local mason = require("mason")
+		local lspconfig = require("lspconfig")
+		local mason_lspconfig = require("mason-lspconfig")
+
+		mason.setup()
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"lua_ls",
+				"csharp_ls",
+				"marksman"
+			},
+		})
+
+		lspconfig.lus_ls.setup({})
+		lspconfig.csharp_ls.setup({})
+		lspconfig.marksman.setup({})
+	end
+}
