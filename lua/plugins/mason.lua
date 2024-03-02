7a1b50e6739a170bf05dea76fda1f6ec8111d5b5
tonightoo <tonightoo.development@gmail.com>
2024-03-03 02:36:59 +0900

    made lsp available

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,23 +1,66 @@
 return {
-	'williamboman/mason.nvim',
-	'williamboman/mason-lspconfig.nvim',
-	'neovim/nvim-lspconfig',
-	config = function()
-		local mason = require('mason')
-		local lspconfig = require('lspconfig')
-		local mason_lspconfig = require('mason-lspconfig')
+	{
+		'williamboman/mason.nvim',
+		build = ':MasonUpdate',
+		opts = {},
+	},
+	{
+		'williamboman/mason-lspconfig.nvim',
+		dependencies = {
+			{ 'williamboman/mason.nvim' },
+			{ 'neovim/nvim-lspconfig' },
+		},
+		config = function()
+			local mason = require('mason')
+			local lspconfig = require('lspconfig')
+			local mason_lspconfig = require('mason-lspconfig')
 
-		mason.setup()
-		lspconfig.setup()
-		mason_lspconfig.setup()
-		mason_lspconfig.setup_handlers({
-			function(server_name)
-				lspconfig[server_name].setup({})
-			end
-		})
+			mason.setup()
+			mason_lspconfig.setup()
+			mason_lspconfig.setup_handlers({
+				function(server_name)
+					lspconfig[server_name].setup({})
+				end
+			})
 
-		-- lspconfig.lua_ls.setup({})
-		-- lspconfig.csharp_ls.setup({})
-		-- lspconfig.marksman.setup({})
-	end
+			-- keymap
+			vim.api.nvim_create_autocmd('LspAttach', {
+				group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+				callback = function(ev)
+					vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+					local opts = { buffer = ev.buf }
+					vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+					vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+					vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+					vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+					vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+					vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+					vim.keymap.set('n', '<space>wl', function()
+						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					end, opts)
+					vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+					vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+					vim.keymap.set({'n', 'v'}, '<space>ca', vim.lsp.buf.code_action, opts)
+					vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+					vim.keymap.set('n', '<space>f', function()
+						vim.lsp.buf.format { async = true }
+					end, opts)
+				end,
+			})
+
+			-- language settings
+			-- suppress "warning: undefined global 'vim'"
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = {'vim'},
+						}
+					}
+				}
+			})
+		end
+	},
 }
