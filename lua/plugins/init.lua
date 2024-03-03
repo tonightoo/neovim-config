f07c54608fe1e4ce90f795b84b94f33a25a18534
tonightoo <tonightoo.development@gmail.com>
2024-03-03 17:14:35 +0900

    add autopairs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,4 +2,9 @@ return {
 	'christoomey/vim-tmux-navigator',
 	'nvim-lua/plenary.nvim',
 	{'vim-jp/vimdoc-ja', ft = 'help'},
+	{
+		'windwp/nvim-autopairs',
+		event = 'InsertEnter',
+		config = true,
+	}
 }
