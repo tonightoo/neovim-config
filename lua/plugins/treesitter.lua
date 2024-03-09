96174fb57d156945b1a85c6ab458ef1e64efdd51
tonightoo <tonightoo.development@gmail.com>
2024-03-09 23:49:03 +0900

    add treesitter auto install setting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,5 +3,10 @@ return {
 	build = ':TSUpdate',
 	config = function()
 		require('nvim-treesitter.install').update({ with_sync = true })()
+		require('nvim-treesitter.configs').setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
 	end
 }
